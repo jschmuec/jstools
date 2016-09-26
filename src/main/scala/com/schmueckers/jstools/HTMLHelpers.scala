002cd0b30016a1503f6b02bcbb01f9fc821e7c89
js <jschmuec@gmail.com>
2016-09-26 23:28:41 +0200

    added functions that help to iterate over directories, similar to UNIX find command.

--- src/main/scala/com/schmueckers/jstools/HTMLHelpers.scala
+++ src/main/scala/com/schmueckers/jstools/HTMLHelpers.scala
@@ -2,25 +2,25 @@ package com.schmueckers.jstools
 
 /** Defines various stuff that makes working with HTML easier
   */
-object HTMLHelpers {
+package object HTMLHelpers {
   /** Defines a method [[#toHtml]] that translates a table into an HTML table
     */
   implicit class TableHelper[H, V](t: Table[H, V]) {
     def toHtml = {
       def v = <table>
-                <thead>
-                  <tr>
-                    { for (h <- t.headers) yield <th>{ h }</th> }
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    for (r <- t.rows) yield <tr>
-                                              { for (f <- r) yield <td>{ f.getOrElse("&nbsp;") }</td> }
-                                            </tr>
-                  }
-                </tbody>
-              </table>
+      <thead>
+      <tr>
+      { for (h <- t.headers) yield <th>{ h }</th> }
+      </tr>
+      </thead>
+      <tbody>
+      {
+        for (r <- t.rows) yield <tr>
+        { for (f <- r) yield <td>{ f.getOrElse("&nbsp;") }</td> }
+        </tr>
+      }
+      </tbody>
+      </table>
       v
     }
   }
---
+++ src/main/scala/com/schmueckers/jstools/files.scala
@@ -0,0 +1,42 @@
+package com.schmueckers.jstools
+
+import java.nio.file.FileSystems
+import scala.collection.JavaConverters._
+import java.nio.file.Path
+import java.io.File
+
+package object files {
+
+  /** Implicit conversion so that the Path logic isn't
+    * exposed to the outside code
+    */
+  implicit def toPath(filename: String) =
+    FileSystems.getDefault.getPath(filename)
+
+  /** Recursively goes through the directory and finds all files inside it
+    * including directories
+    */
+  def find(dir: Path, includeHidden: Boolean = false): Iterator[Path] =
+    java.nio.file.Files.walk(dir).iterator().asScala.filter(p => includeHidden || (!p.isHidden))
+
+  def findRelative(dir: Path): Iterator[Path] =
+    find(dir).map(dir.relativize(_)) // drop the actual file
+
+  /** Recursively goes through the directory and finds all the regular files. So
+    * directories and stuff will be excluded.
+    */
+  def findRegularFiles(dirName: String) = find(dirName).filter(_.isRegularFile)
+
+  /** Adds a check to a Path that tells us if it is a regular file
+    */
+  implicit class PathImplicits(path: Path) {
+    def isRegularFile = java.nio.file.Files.isRegularFile(path)
+    def isHidden = java.nio.file.Files.isHidden(path)
+  }
+
+  /** Adds a predicate to check if a file is a regular file
+    */
+  implicit class FileImplicits(f: File) {
+    def isRegularFile = f.toPath().isRegularFile
+  }
+}
