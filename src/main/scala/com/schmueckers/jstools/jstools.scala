236bc2ad4267ebcbd9d389dd281c2ccec62d0de2
js <jschmuec@gmail.com>
2016-10-01 00:11:37 +0200

    add implicit conversion from Option[A] to Try[A]

---
+++ src/main/scala/com/schmueckers/jstools/jstools.scala
@@ -0,0 +1,21 @@
+package com.schmueckers
+
+import scala.util.Success
+import scala.util.Failure
+import scala.util.Try
+
+package object jstools {
+  /** Implements the F# pipe operator.
+    *
+    * It's not really that useful given that methods
+    * can't be called and many functions in scala are just methods.
+    */
+  implicit class Pipe[A](a: A) {
+    def |>[B](f: A => B) = f(a)
+  }
+
+  implicit def optionToTry[A]( o : Option[A] ) : Try[A] =o match {
+    case Some(a) => Success(a)
+    case None    => Failure(new NoSuchElementException)
+  }
+}
