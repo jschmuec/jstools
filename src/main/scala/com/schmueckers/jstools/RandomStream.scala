aeda986567ba9a468524767cd1b5f45da5eaa445
js <jschmuec@gmail.com>
2017-08-25 13:35:05 +0200

    Renamed RandStream to RandomStream

---
+++ src/main/scala/com/schmueckers/jstools/RandomStream.scala
@@ -0,0 +1,34 @@
+package com.schmueckers.jstools
+
+import java.util.Random
+
+/**
+  * Various methods to create streams of random numbers
+  *
+  * This is a fully functional implementation because as long as you
+  * keep track of the head of the stream it will just be there.
+  */
+object RandomStream {
+  /**
+    * A function which actually repeatedly calls the method passed
+    * in on the random number generator and thereby creates a stream
+    * of random elements of the given type. In practice use the Int()
+    * and Int( bound : Int ) methods to get a stream of the right
+    * type.
+    *
+    * @param f The function on java.util.Random that should be invoked to pull
+    *          the next element out of the generator
+    * @tparam T The type of the value to be returned in the stream
+    */
+  def create[T](f: (Random) => T) = {
+    val rand = new java.util.Random
+
+    def get_next: Stream[T] =
+      f(rand) #:: get_next
+    get_next
+  }
+
+  // convenience methods and sample implementations
+  def Int = create( (r : java.util.Random ) => r.nextInt )
+  def BoundInt( bound : Int ) = create( (r: java.util.Random) => r.nextInt( bound ) )
+}
